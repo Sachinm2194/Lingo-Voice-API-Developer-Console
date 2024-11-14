1509a635dbcc5aa14022109d68f5905b7e18875a
Sachin M Mandave <sachinmandave@sandlogic.in>
2024-11-14 17:01:25 +0530

    added sidebar

--- Lingo-Voice-API-Console/src/app/shared/Services/ThemeServices/themes.service.ts
+++ Lingo-Voice-API-Console/src/app/shared/Services/ThemeServices/themes.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemesService {
   private currentTheme: string;
+  private themeSubject: BehaviorSubject<string>; // Observable for theme changes
 
   constructor() {
     this.currentTheme = localStorage.getItem('theme') || 'light-theme';
+    this.themeSubject = new BehaviorSubject<string>(this.currentTheme); // Initialize with the current theme
     this.applyTheme(this.currentTheme);
   }
 
@@ -15,6 +18,7 @@ export class ThemesService {
     this.currentTheme = theme;
     localStorage.setItem('theme', theme);
     this.applyTheme(theme);
+    this.themeSubject.next(theme); // Emit the new theme to subscribers
   }
 
   applyTheme(theme: string): void {
@@ -24,5 +28,9 @@ export class ThemesService {
 
   getTheme(): string {
     return this.currentTheme;
+  }
+
+  getThemeObservable() {
+    return this.themeSubject.asObservable(); // Allow components to subscribe to theme changes
   }
 }
